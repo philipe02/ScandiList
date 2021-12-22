7abd5934d256c27c466169c28028653c62510bb9
Philipe Santos <philipe_02@hotmail.com>
2021-12-22 17:49:31 -0300

    changing api url to https, icon and title

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://scandilist-api.herokuapp.com/',
+    baseURL: 'https://scandilist-api.herokuapp.com/',
     timeout: 30000
 })
 
